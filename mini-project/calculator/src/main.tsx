5223a41cb54a99991f44318db6d9c25a5de91c27
JayEsEichi <wlstpgns51@naver.com>
2025-10-21 17:22:20 +0900

    commit

--- mini-project/calculator/src/main.tsx
+++ mini-project/calculator/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import RoutesSetup from "./routes/Routes.tsx";
 import NavBar from "./components/NavBar.tsx";
--- mini-project/calculator/src/routes/Routes.tsx
+++ mini-project/calculator/src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Join from "../pages/Join";
