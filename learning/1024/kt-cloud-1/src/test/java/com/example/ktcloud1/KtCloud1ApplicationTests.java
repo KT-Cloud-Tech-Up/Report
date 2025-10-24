93bc53aa0542b30913de2a5561147e036568d144
JayEsEichi <wlstpgns51@naver.com>
2025-10-24 13:13:39 +0900

    commit

---
+++ learning/1024/kt-cloud-1/src/test/java/com/example/ktcloud1/KtCloud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktcloud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtCloud1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
